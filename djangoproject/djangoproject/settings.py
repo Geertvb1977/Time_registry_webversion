bab69e5d23ff65eda1ad8b29554d307b75c3bdc7
Geert Van Buggenhout <geert_vanbuggenhout@yahoo.com>
2026-01-27 13:39:42 +0100

    Fix timezone - set to Europe/Brussels (CET/CEST)

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -123,9 +123,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'nl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Brussels'
 
 USE_I18N = True
 
